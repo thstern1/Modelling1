f03461acd7854e5317847abdaf4f38b380a482ae
tristanstern <thstern.ieu2024@student.ie.edu>
2026-03-13 13:09:10 +0100

    First session Modelling

--- color.point.py
+++ color.point.py
@@ -9,6 +9,19 @@ class ColorPoint(Point):
         self.y = y
         self.color = color
 
+    def __str__(self):
+        """
+        Overwrite this method to return a string representation of the point
+        :return: string representation of the point
+        """
+        return f"{self.color}: P<{self.x},{self.y}>"
+
 p1 = ColorPoint(1, 2, "red")
+p2 = ColorPoint(3, 4, "green")
+p3 = ColorPoint(5, 6, "blue")
+p4 = ColorPoint(-2, -3, "yellow")
+color_points = [p1, p2, p3, p4]
 print(p1.color)
 print(p1)
+print(color_points)
+
