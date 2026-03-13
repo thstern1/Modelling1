3576898ec5d7158353e0bf35927b99a35ea2c539
tristanstern <thstern.ieu2024@student.ie.edu>
2026-03-13 12:58:08 +0100

    First session Modelling

---
+++ color.point.py
@@ -0,0 +1,14 @@
+from point import Point
+
+class Color(Point):
+    """
+    Color point class inheriting from Point class
+    """
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+p1 = ColorPoint(1, 2, "red")
+print(p1.color)
+print(p1)
--- point.py
+++ point.py
@@ -1,3 +1,4 @@
+import math
 
 class Point:
     """
@@ -9,18 +10,60 @@ class Point:
         :param x: x coordinate of point.
         :param y: y coordinate of point.
         """
-        self.x = x # x is a class attribute
+        self.x = x
         self.y = y
+
     def __str__(self):
         """
         String representation of Point class.
-        :return: String representation of Point class.
+        :return: string representation of Point class.
         """
         return f"P<{self.x},{self.y}>"
 
+    def __repr__(self):
+        return self.__str__()
+
+    def distance_origin(self):
+        """
+        Calculate the distance between point and origin.
+        :return: float, distance between point and origin.
+        """
+        return math.sqrt((self.x)**2 + (self.y)**2)
+
+    def distance_to(self, point):
+        """
+        Calculate the distance between current point and another point.
+        :param point: the other point to calculate distance to.
+        :return: float, distance between current point and another point.
+        """
+        return math.sqrt((self.x - point.x)**2 + (self.y - point.y)**2)
+
+    def __lt__(self, other):
+        """
+        Return True if the point is less than another point.
+        :param other: the other point to compare against.
+        :return: True or False.
+        """
+        return self.distance_origin() < other.distance_origin()
+
+    def __gt__(self, other):
+        """
+        Return True if the point is greater than another point.
+        :param other: the other point to compare against.
+        :return: True or False.
+        """
+        return self.distance_origin() > other.distance_origin()
+
 p1 = Point(1, 2)
 p2 = Point(3, 4)
+p3 = Point(12, 5)
 
-print(p1.x, p1.y)
-print(p2.x, p2.y)
-print(p1)
+print(p1.x, p1.y, p2.x, p2.y)
+print(p1, p2) # P<1,2>
+print(f"{p2} distance to origin is {p2.distance_origin()}")
+print(f"Distance between {p1} and {p2} is {p1.distance_to(p2)}")
+
+points = [p1, p2, p3, Point(15, 6)]
+print(points)
+points.sort()
+print(points)
